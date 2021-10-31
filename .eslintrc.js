c5948d782229fe3f7209bbf2cbcb272ee0998eb0
Daniel Nalborczyk <dnalborczyk@gmail.com>
2021-10-31 12:12:30 -0400

    chore: add graphql eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,28 @@
 'use strict'
 
 module.exports = {
-  extends: [
-    'airbnb-base',
-    'airbnb-typescript/base',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
+  overrides: [
+    {
+      extends: ['plugin:prettier/recommended'],
+      files: ['*.graphql'],
+      parser: '@graphql-eslint/eslint-plugin',
+      plugins: ['@graphql-eslint'],
+    },
+    {
+      extends: [
+        'airbnb-base',
+        'airbnb-typescript/base',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:prettier/recommended',
+      ],
+      files: ['*.js', '*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+      plugins: ['@typescript-eslint'],
+    },
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: ['./tsconfig.json'],
-  },
-  plugins: ['@typescript-eslint'],
 }
